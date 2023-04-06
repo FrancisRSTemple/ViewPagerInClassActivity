f9b1467d4a09954a17302b4e8893b8fd4a4e5b24
Francis R Scallatino <tup31915@temple.edu>
2023-04-06 14:58:51 -0400

    setup and onClickListener for the button
    this onClickListener is suppose to change numberOfPages. however, it shouldn't really work because we haven't told the FragmentStateAdapter

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -2,23 +2,34 @@ package edu.temple.viewpagerinclasssctivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
+    private val addButton: Button by lazy {
+        findViewById(R.id.addFragmentButton)
+    }
+
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.activityViewPager)
     }
+
+    var numberOfPAges = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        addButton.setOnClickListener{
+            numberOfPAges++
+        }
+
         //need to pass the reference to the thing that has the ViewPager2
         viewPager.adapter = object : FragmentStateAdapter(this){
             //number of fragments you want to process
-            override fun getItemCount() = 10
+            override fun getItemCount() = numberOfPAges
 
             //returns the fragment at this "position"
             override fun createFragment(position: Int) = TextFragment.newInstance((position+1).toString())
